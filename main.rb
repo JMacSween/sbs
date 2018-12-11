238c5835b5ed981e1ce831a8752d799f495715b1
Joshua MacSween <joshmacsween@gmail.com>
2018-12-11 16:02:56 -0700

    5.7

--- main.rb
+++ main.rb
@@ -1,14 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+
 get '/' do
   erb :home
 end
 
 get '/about' do
+  @title = "All about this Website "
   erb :about
 end
+
 get '/contact' do
+  @title = "Call Me Anytime"
   erb :contact
+end
+
+not_found do
+  erb :not_found
 end
 
 set :public_folder, 'public'
